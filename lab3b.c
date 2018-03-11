1e2fc131c5926d947f6284feda7017ef846cd623
Christopher Ngai <christopherngai@Christophers-MacBook-Pro.local>
2018-03-10 19:19:28 -0800

    main function

--- lab3b.c
+++ lab3b.c
@@ -1,2 +1,19 @@
 // NAME: Virgil Jose, Christopher Ngai
-// EMAIL:
+// EMAIL: virgil@ucla.edu, cngai1223@gmail.com
+// ID: 904765891, 404795904
+
+int main(int argc, char * argv[]){
+	/* check to make sure correct number of arguments */
+    if (argc != 2){
+    	fprintf(stderr, "Error: incorrect number of arguments.\n");
+    	exit(1);
+    }
+    /* open the file system image */
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0){
+    	fprintf(stderr, "Error opening file system.\n");
+    	exit(1);
+    }
+
+    exit(0);
+}
