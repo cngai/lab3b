611ea0173e42aacce0297cfb93e04f63fc0339a8
Virgil Jose <virgil@ucla.edu>
2018-03-10 19:32:32 -0800

    wrapper fctns

--- lab3b.c
+++ lab3b.c
@@ -2,6 +2,18 @@
 // EMAIL: virgil@ucla.edu, cngai1223@gmail.com
 // ID: 904765891, 404795904
 
+void check_dirs() {
+    return;
+}
+
+void check_inodes() {
+    return;
+}
+
+void check_blocks() {
+    return;
+}
+
 int main(int argc, char * argv[]){
 	/* check to make sure correct number of arguments */
     if (argc != 2){
@@ -15,5 +27,9 @@ int main(int argc, char * argv[]){
     	exit(1);
     }
 
+    check_blocks();
+    check_inodes();
+    check_dirs();
+    
     exit(0);
 }
